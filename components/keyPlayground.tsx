9dc3aeb0e9d5e19423a5a094410541ce6f074d5c
MichaelUnkey <michael@unkey.dev>
2024-05-22 12:48:49 -0400

    layout fixes in steps

--- components/keyPlayground.tsx
+++ components/keyPlayground.tsx
@@ -6,73 +6,59 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 import { handleCurlServer } from "../lib/helper";
 import { type stepDataType, data } from "@/lib/data";
-import { Step1, Step2, Step3, Step4, Step5, Step6, Step7, Step8, Step9, Step10, Step11 } from "./stepComponents";
-
+import {
+  Step1,
+  Step2,
+  Step3,
+  Step4,
+  Step5,
+  Step6,
+  Step7,
+  Step8,
+  Step9,
+  Step10,
+  Step11,
+} from "./stepComponents";
 
 export default function KeyPlayground() {
   const apiId = process.env.NEXT_PUBLIC_UNKEY_API_ID;
   const [stepData, setStepData] = useState<stepDataType>(data);
-  // Curl Commands
   const [curlResponse, setCurlResponse] = useState<string>("");
   const [curlString, setCurlString] = useState<string>("");
   const [renderString, setRenderString] = useState<string>("");
-  // Step Data
   const [step, setStep] = useState<number>(1);
-  // Shared Data
-const [timeStamp, setTimeStamp] = useState<number>(0);
+  const [timeStamp, setTimeStamp] = useState<number>(0);
   const [keyId, setKeyId] = useState<string>("");
   const [keyName, setKeyName] = useState<string>("");
-  // Router
-  // const router = useRouter();
-  // const pathname = usePathname();
-  // const searchParams = useSearchParams();
   const [isMounted, setIsMounted] = useState(false);
-  // const createQueryString = useCallback(
-  //   (name: string, value: string) => {
-      
-  //     const params = new URLSearchParams(searchParams.toString());
-  //     params.set(name, value);
-  //     return params.toString();
-  //   },
-  //   [searchParams]
-  // );
-
 
   useEffect(() => {
-    if(!isMounted) {
+    if (!isMounted) {
       setTimeStamp(Date.now() + 24 * 60 * 60 * 1000);
-    };
+    }
     setIsMounted(true);
   }, [isMounted]);
-
-  
-  // useEffect(() => {
-  //   const params = new URLSearchParams(searchParams.toString());
-  //   params.get("step")
-  //     ? setStep(parseInt(params.get("step") as string))
-  //     : setStep(1);
-  // }, [pathname, searchParams]);
 
   function handleSteps(step: number) {
     setStep(step);
   }
 
- 
-
-  const parseCurlCommand = useCallback((stepString: string) => {
-    let tempString = stepString;
-    
-    tempString = tempString.replace("<timeStamp>", timeStamp.toString());
-    if (apiId) {
+  const parseCurlCommand = useCallback(
+    (stepString: string) => {
+      let tempString = stepString;
+      tempString = tempString.replace("<timeStamp>", timeStamp.toString());
+      if (apiId) {
+        tempString =
+          apiId.length > 0 ? tempString.replace("<apiId>", apiId) : tempString;
+      }
       tempString =
-        apiId.length > 0 ? tempString.replace("<apiId>", apiId) : tempString;
-    }
-    tempString =
-      keyId.length > 0 ? tempString.replace("<keyId>", keyId) : tempString;
-    tempString =
-      keyName.length > 0 ? tempString.replace("<key>", keyName) : tempString;
-    return tempString;
-  }, [apiId, keyId, keyName, timeStamp]);
+        keyId.length > 0 ? tempString.replace("<keyId>", keyId) : tempString;
+      tempString =
+        keyName.length > 0 ? tempString.replace("<key>", keyName) : tempString;
+      return tempString;
+    },
+    [apiId, keyId, keyName, timeStamp]
+  );
 
   async function handleButtonClick(index: number) {
     let tempString = stepData[index]?.curlCommand ?? "";
@@ -107,64 +93,71 @@ const [timeStamp, setTimeStamp] = useState<number>(0);
   async function handleTerminalRequest(curlString: string) {
     handleCurl(curlString);
   }
-  const handleRender = useCallback((index: number) => {
-    if (stepData) {
-      let tempString = stepData[index].curlCommand ?? "";
-      const curlString = parseCurlCommand(tempString);
-      setRenderString(curlString);
-    }
-  }, [ parseCurlCommand, stepData]);
-  
-  // useEffect(() => {
-  //   handleRender(step);
-  // }, [handleRender, step]);
+  const handleRender = useCallback(
+    (index: number) => {
+      if (stepData) {
+        let tempString = stepData[index].curlCommand ?? "";
+        const curlString = parseCurlCommand(tempString);
+        setRenderString(curlString);
+      }
+    },
+    [parseCurlCommand, stepData]
+  );
 
-  
   return !stepData ? (
     <div>Loading...</div>
   ) : (
     <div className="flex flex-col text-white/60">
-      {Step1(step, apiId ?? "", stepData, handleButtonClick)}
-      {Step2(step, keyId, stepData, renderString, handleButtonClick)}
-      {Step3(
-        step,
-        apiId ?? "",
-        keyId,
-        stepData,
-        renderString,
-        handleButtonClick
-      )}
-      {Step4(step, keyId, stepData, renderString, handleButtonClick)}
-      {Step5(
-        step,
-        stepData,
-        renderString,
-        handleButtonClick,
-        apiId ?? "",
-        keyName
-      )}
-      {Step6(step, timeStamp, keyId, stepData, renderString, handleButtonClick)}
-      {Step7(
-        step,
-        keyId,
-        stepData,
-        renderString,
-        apiId ?? "",
-        keyName,
-        handleButtonClick
-      )}
-      {Step8(step, keyId, stepData, renderString, handleButtonClick)}
-      {Step9(step, keyId, stepData, renderString, handleButtonClick)}
-      {Step10(
-        step,
-        apiId ?? "",
-        keyName,
-        stepData,
-        renderString,
-        handleButtonClick
-      )}
-      {Step11(step, stepData)}
-      <div className="mt-10 h-96">
+      <div className="flex flex-row w-full">
+        {Step1(step, apiId ?? "", stepData, handleButtonClick)}
+        {Step2(step, keyId, stepData, renderString, handleButtonClick)}
+        {Step3(
+          step,
+          apiId ?? "",
+          keyId,
+          stepData,
+          renderString,
+          handleButtonClick
+        )}
+        {Step4(step, keyId, stepData, renderString, handleButtonClick)}
+        {Step5(
+          step,
+          stepData,
+          renderString,
+          handleButtonClick,
+          apiId ?? "",
+          keyName
+        )}
+        {Step6(
+          step,
+          timeStamp,
+          keyId,
+          stepData,
+          renderString,
+          handleButtonClick
+        )}
+        {Step7(
+          step,
+          keyId,
+          stepData,
+          renderString,
+          apiId ?? "",
+          keyName,
+          handleButtonClick
+        )}
+        {Step8(step, keyId, stepData, renderString, handleButtonClick)}
+        {Step9(step, keyId, stepData, renderString, handleButtonClick)}
+        {Step10(
+          step,
+          apiId ?? "",
+          keyName,
+          stepData,
+          renderString,
+          handleButtonClick
+        )}
+        {Step11(step, stepData)}
+      </div>
+      <div className="flex flex-col mt-10 h-96">
         <TerminalProvider>
           <Terminal
             sendRequest={(curl: string) => handleTerminalRequest(curl)}
@@ -177,17 +170,3 @@ const [timeStamp, setTimeStamp] = useState<number>(0);
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- components/stepComponents.tsx
+++ components/stepComponents.tsx
@@ -10,60 +10,61 @@ export function Step1(
   stepData: stepDataType,
   handleButtonClick: (index: number) => void
 ) {
-  return (
-    <div
-      key="step1"
-      className={cn(step === 1 ? "flex flex-col" : "hidden", "gap-4 mt-10")}
-    >
-      <h2 className="text-lg mt-6">Welcome to the Unkey API playground</h2>
-      <p className="text-md mt-2">
-        Here you can test out the Unkey API using curl commands to make Unkey
-        API calls.
-      </p>
-      <h2 className="text-2xl">Create a first Key</h2>
-      <p>
-        Directions: Type commands in manualy, copy and paste, or just click the
-        &quot;Send request&quot; button under each step. Leave the{" "}
-        <span className=" text-white">&lt;token&gt;</span> tag for now. Normally
-        you would put your root key here. We will handle any rootkey
-        requirements in this playground.
-      </p>
-      <p>{stepData[1].blurb}</p>
-      <p>
-        See{" "}
-        <Link
-          className="text-white underline hover:text-white/60"
-          href="https://www.unkey.com/docs/api-reference/keys/create"
-        >
-          create key docs.
-        </Link>
-      </p>
-      <p>
-        API ID: <span className="my-2 text-white">{apiId}</span>
-      </p>
+  if (step === 1) {
+    return (
+      <div key="step1" className={cn("flex flex-col gap-4 mt-10")}>
+        <h2 className="text-lg mt-6">Welcome to the Unkey API playground</h2>
+        <p className="text-md mt-2">
+          Here you can test out the Unkey API using curl commands to make Unkey
+          API calls.
+        </p>
+        <h2 className="text-2xl">Create a first Key</h2>
+        <p>
+          Directions: Type commands in manualy, copy and paste, or just click
+          the &quot;Send request&quot; button under each step. Leave the{" "}
+          <span className=" text-white">&lt;token&gt;</span> tag for now.
+          Normally you would put your root key here. We will handle any rootkey
+          requirements in this playground.
+        </p>
+        <p>{stepData[1].blurb}</p>
+        <p>
+          See{" "}
+          <Link
+            className="text-white underline hover:text-white/60"
+            href="https://www.unkey.com/docs/api-reference/keys/create"
+          >
+            create key docs.
+          </Link>
+        </p>
+        <p>
+          API ID: <span className="my-2 text-white">{apiId}</span>
+        </p>
 
-      <div className="my-8">
-        <CodeBlock>{stepData[1].curlCommand}</CodeBlock>
-      </div>
-      <div className="flex justify-end">
-        <Button
-          className="lg:w-1/4"
-          onClick={() => handleButtonClick(1)}
-          variant={"ghost"}
-        >
-          <div className="w-full p-px rounded-lg h-10 bg-gradient-to-r from-[#02DEFC] via-[#0239FC] to-[#7002FC] overflow-hidden">
-            <div className="bg-black rounded-[7px] h-full bg-opacity-95 hover:bg-opacity-25 duration-1000">
-              <div className="flex items-center justify-center w-full h-full bg-gradient-to-tr from-[#02DEFC]/20 via-[#0239FC]/20 to-[#7002FC]/20  rounded-[7px]">
-                <span className="text-sm font-semibold text-white">
-                  Send request
-                </span>
+        <div className="my-8">
+          <CodeBlock>{stepData[1].curlCommand}</CodeBlock>
+        </div>
+        <div className="flex justify-end">
+          <Button
+            className="lg:w-1/4"
+            onClick={() => handleButtonClick(1)}
+            variant={"ghost"}
+          >
+            <div className="w-full p-px rounded-lg h-10 bg-gradient-to-r from-[#02DEFC] via-[#0239FC] to-[#7002FC] overflow-hidden">
+              <div className="bg-black rounded-[7px] h-full bg-opacity-95 hover:bg-opacity-25 duration-1000">
+                <div className="flex items-center justify-center w-full h-full bg-gradient-to-tr from-[#02DEFC]/20 via-[#0239FC]/20 to-[#7002FC]/20  rounded-[7px]">
+                  <span className="text-sm font-semibold text-white">
+                    Send request
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        </Button>
+          </Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
 
 export function Step2(
@@ -73,38 +74,39 @@ export function Step2(
   renderString: string,
   handleButtonClick: (index: number) => void
 ) {
-  return (
-    <div
-      key="step2"
-      className={cn(step === 2 ? "flex flex-col" : "hidden", "gap-4 mt-12")}
-    >
-      <h2>Get Key Data</h2>
-      <p>{stepData[2].blurb}</p>
-      <p>
-        keyId: <span className="my-2 text-white">{keyId}</span>
-      </p>
-      <div className="mt-8">
-      <CodeBlock>{renderString}</CodeBlock>
-      </div>
-      <div className="flex justify-end">
-        <Button
-          className="lg:w-1/4"
-          onClick={() => handleButtonClick(2)}
-          variant={"ghost"}
-        >
-          <div className="w-full p-px rounded-lg h-10 bg-gradient-to-r from-[#02DEFC] via-[#0239FC] to-[#7002FC] overflow-hidden">
-            <div className="bg-black rounded-[7px] h-full bg-opacity-95 hover:bg-opacity-25 duration-1000">
-              <div className="flex items-center justify-center w-full h-full bg-gradient-to-tr from-[#02DEFC]/20 via-[#0239FC]/20 to-[#7002FC]/20  rounded-[7px]">
-                <span className="text-sm font-semibold text-white">
-                  Send request
-                </span>
+  if (step === 2) {
+    return (
+      <div key="step2" className={cn("flex flex-col gap-4 mt-12 w-full")}>
+        <h2>Get Key Data</h2>
+        <p>{stepData[2].blurb}</p>
+        <p>
+          keyId: <span className="my-2 text-white">{keyId}</span>
+        </p>
+        <div className="mt-8">
+          <CodeBlock>{renderString}</CodeBlock>
+        </div>
+        <div className="flex justify-end">
+          <Button
+            className="lg:w-1/4"
+            onClick={() => handleButtonClick(2)}
+            variant={"ghost"}
+          >
+            <div className="w-full p-px rounded-lg h-10 bg-gradient-to-r from-[#02DEFC] via-[#0239FC] to-[#7002FC] overflow-hidden">
+              <div className="bg-black rounded-[7px] h-full bg-opacity-95 hover:bg-opacity-25 duration-1000">
+                <div className="flex items-center justify-center w-full h-full bg-gradient-to-tr from-[#02DEFC]/20 via-[#0239FC]/20 to-[#7002FC]/20  rounded-[7px]">
+                  <span className="text-sm font-semibold text-white">
+                    Send request
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        </Button>
+          </Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
 
 export function Step3(
@@ -115,41 +117,45 @@ export function Step3(
   renderString: string,
   handleButtonClick: (index: number) => void
 ) {
-  return (
-    <div
-      key="step3"
-      className={cn(step === 3 ? "flex" : "hidden", "gap-4 mt-12")}
-    >
-      <h2>Verify Key</h2>
-      <p>{stepData[3].blurb}</p>
-      <p>
-        API ID: <span className="my-2 text-white">{apiId}</span>
-      </p>
-      <p>
-        keyId: <span className="my-2 text-white">{keyId}</span>
-      </p>
-      <div className="mt-8">
-        <CodeBlock>{renderString}</CodeBlock>
-      </div>
-      <div className="flex justify-end">
-        <Button
-          className="w-92"
-          onClick={() => handleButtonClick(2)}
-          variant={"ghost"}
-        >
-          <div className="w-full p-px rounded-lg h-10 bg-gradient-to-r from-[#02DEFC] via-[#0239FC] to-[#7002FC] overflow-hidden">
-            <div className="bg-black rounded-[7px] h-full bg-opacity-95 hover:bg-opacity-25 duration-1000">
-              <div className="flex items-center justify-center w-full h-full bg-gradient-to-tr from-[#02DEFC]/20 via-[#0239FC]/20 to-[#7002FC]/20  rounded-[7px]">
-                <span className="text-sm font-semibold text-white">
-                  Send request
-                </span>
+  if (step === 3) {
+    return (
+      <div
+        key="step3"
+        className={cn("flex flex-col gap-4 mt-12 w-full")}
+      >
+        <h2>Verify Key</h2>
+        <p>{stepData[3].blurb}</p>
+        <p>
+          API ID: <span className="my-2 text-white">{apiId}</span>
+        </p>
+        <p>
+          keyId: <span className="my-2 text-white">{keyId}</span>
+        </p>
+        <div className="mt-8">
+          <CodeBlock>{renderString}</CodeBlock>
+        </div>
+        <div className="flex justify-end">
+          <Button
+            className="lg:w-1/4"
+            onClick={() => handleButtonClick(2)}
+            variant={"ghost"}
+          >
+            <div className="w-full p-px rounded-lg h-10 bg-gradient-to-r from-[#02DEFC] via-[#0239FC] to-[#7002FC] overflow-hidden">
+              <div className="bg-black rounded-[7px] h-full bg-opacity-95 hover:bg-opacity-25 duration-1000">
+                <div className="flex items-center justify-center w-full h-full bg-gradient-to-tr from-[#02DEFC]/20 via-[#0239FC]/20 to-[#7002FC]/20  rounded-[7px]">
+                  <span className="text-sm font-semibold text-white">
+                    Send request
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        </Button>
+          </Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
 
 export function Step4(
@@ -158,16 +164,16 @@ export function Step4(
   stepData: stepDataType,
   renderString: string,
   handleButtonClick: (index: number) => void
-) {
+) {if (step === 4) {
   return (
-    <div key="step4" className={cn(step === 4 ? "flex flex-col" : "hidden")}>
+    <div key="step4" className={cn("flex flex-col gap-4 mt-12 w-full")}>
       <h2>Update Key</h2>
       <p>{stepData[4].blurb}</p>
       <p>
-        ownerId: <span className="my-2 text-violet-400">user_1234</span>
+        ownerId: <span className="my-2 text-white">user_1234</span>
       </p>
       <p>
-        keyId: <span className="my-2 text-violet-400">{keyId}</span>
+        keyId: <span className="my-2 text-white">{keyId}</span>
       </p>
       <div className="my-8">
         <CodeBlock className="">{renderString}</CodeBlock>
@@ -190,7 +196,9 @@ export function Step4(
         </Button>
       </div>
     </div>
-  );
+  );} else {
+    return null;
+  }
 }
 
 export function Step5(
@@ -201,15 +209,16 @@ export function Step5(
   apiId: string,
   keyName: string
 ) {
+  if (step === 5) {
   return (
-    <div key="step5" className={cn(step === 5 ? "flex-col" : "hidden")}>
+    <div key="step5" className={cn("flex flex-col gap-4 mt-12 w-full")}>
       <h2>Verify Key</h2>
       <p>{stepData[5].blurb}</p>
       <p>
-        API ID: <span className="my-2 text-violet-400">{apiId}</span>
+        API ID: <span className="my-2 text-white">{apiId}</span>
       </p>
       <p>
-        Key name: <span className="my-2 text-violet-400">{keyName}</span>
+        Key name: <span className="my-2 text-white">{keyName}</span>
       </p>
       <div className="my-8">
         <CodeBlock className="">{renderString}</CodeBlock>
@@ -232,7 +241,9 @@ export function Step5(
         </Button>
       </div>
     </div>
-  );
+  );} else {
+    return null;
+  }
 }
 
 export function Step6(
@@ -242,29 +253,29 @@ export function Step6(
   stepData: stepDataType,
   renderString: string,
   handleButtonClick: (index: number) => void
-) {
+) {if (step === 6) {
   return (
-    <div key="step6" className={cn(step === 6 ? "flex-col" : "hidden")}>
+    <div key="step6" className={cn("flex flex-col gap-4 mt-12 w-full")}>
       <h2>Update expiration</h2>
       <p>{stepData[6].blurb}</p>
       <p>
-        Expiration: <span className="my-2 text-violet-400">{timeStamp}</span>
+        Expiration: <span className="my-2 text-white">{timeStamp}</span>
       </p>
       <p>
-        keyId: <span className="my-2 text-violet-400">{keyId}</span>
+        keyId: <span className="my-2 text-white">{keyId}</span>
       </p>
       <p>
-        url: <span className="my-2 text-violet-400">{stepData[6].url}</span>
+        url: <span className="my-2 text-white">{stepData[6].url}</span>
       </p>
       <p>headers:</p>
       <p className="pl-4">
-        <span className="my-2 text-violet-400">
+        <span className="my-2 text-white">
           {stepData[6]?.headers?.contentType}
         </span>
       </p>
       <p>
         method:{" "}
-        <span className="my-2 text-violet-400">{stepData[6].method}</span>
+        <span className="my-2 text-white">{stepData[6].method}</span>
       </p>
       <div className="my-8">
         <CodeBlock className="">{renderString}</CodeBlock>
@@ -287,7 +298,9 @@ export function Step6(
         </Button>
       </div>
     </div>
-  );
+  );} else {
+    return null;
+  }
 }
 
 export function Step7(
@@ -298,16 +311,16 @@ export function Step7(
   apiId: string,
   keyName: string,
   handleButtonClick: (index: number) => void
-) {
+) {if (step === 7) {
   return (
-    <div key="step7" className={cn(step === 7 ? "flex-col" : "hidden")}>
+    <div key="step7" className={cn("flex flex-col gap-4 mt-12 w-full")}>
       <h2>Verify Key</h2>
       <p>{stepData[7].blurb}</p>
       <p>
-        API ID: <span className="my-2 text-violet-400">{apiId}</span>
+        API ID: <span className="my-2 text-white">{apiId}</span>
       </p>
       <p>
-        Key Name: <span className="my-2 text-violet-400">{keyName}</span>
+        Key Name: <span className="my-2 text-white">{keyName}</span>
       </p>
       <div className="my-8">
         <CodeBlock>{renderString}</CodeBlock>
@@ -330,7 +343,9 @@ export function Step7(
         </Button>
       </div>
     </div>
-  );
+  );} else {
+    return null;
+  }
 }
 
 export function Step8(
@@ -339,13 +354,13 @@ export function Step8(
   stepData: stepDataType,
   renderString: string,
   handleButtonClick: (index: number) => void
-) {
+) {if (step === 8) {
   return (
-    <div key="step8" className={cn(step === 8 ? "flex-col" : "hidden")}>
+    <div key="step8" className={cn("flex flex-col gap-4 mt-12 w-full")}>
       <h2> Verify Key</h2>
       <p>{stepData[8].blurb}</p>
       <p>
-        keyId: <span className="my-2 text-violet-400">{keyId}</span>
+        keyId: <span className="my-2 text-white">{keyId}</span>
       </p>
       <div className="my-8">
         <CodeBlock>{renderString}</CodeBlock>
@@ -368,7 +383,9 @@ export function Step8(
         </Button>
       </div>
     </div>
-  );
+  );} else {
+    return null;
+  }
 }
 
 export function Step9(
@@ -377,14 +394,13 @@ export function Step9(
   stepData: stepDataType,
   renderString: string,
   handleButtonClick: (index: number) => void
-) {
+) {if (step === 9) {
   return (
-    <div key="step9" className={cn(step === 9 ? "flex" : "hidden")}>
+    <div key="step9" className={cn("flex flex-col gap-4 mt-12 w-full")}>
       <h1>Delete Key</h1>
-      <div className="flex flex-col gap-4 ">
         <p>{stepData[9].blurb}</p>
         <p>
-          keyId: <span className="my-2 text-violet-400">{keyId}</span>
+          keyId: <span className="my-2 text-white">{keyId}</span>
         </p>
         <div className="my-8">
           <CodeBlock>{renderString}</CodeBlock>
@@ -406,9 +422,10 @@ export function Step9(
             </div>
           </Button>
         </div>
-      </div>
     </div>
-  );
+  );} else {
+    return null;
+  }
 }
 
 export function Step10(
@@ -418,21 +435,20 @@ export function Step10(
   stepData: stepDataType,
   renderString: string,
   handleButtonClick: (index: number) => void
-) {
+) {if (step === 10) {
   return (
-    <div key="step10" className={cn(step === 10 ? "flex" : "hidden")}>
+    <div key="step10" className={cn("flex flex-col gap-4 mt-12 w-full")}>
       <h1>Verify Key</h1>
       <p>{stepData[10].blurb}</p>
       <p>
-        apiId: <span className="my-2 text-violet-400">{apiId}</span>
+        apiId: <span className="my-2 text-white">{apiId}</span>
       </p>
       <p>
-        key: <span className="my-2 text-violet-400">{keyName}</span>
+        key: <span className="my-2 text-white">{keyName}</span>
       </p>
       <div className="my-8">
         <CodeBlock>{renderString}</CodeBlock>
       </div>
-      <div className="flex flex-row justify-end">
         <div className="flex justify-end">
           <Button
             className="lg:w-1/4"
@@ -450,38 +466,40 @@ export function Step10(
             </div>
           </Button>
         </div>
-      </div>
     </div>
-  );
+  );} else {
+    return null;
+  }
 }
 
 export function Step11(step: number, stepData: stepDataType) {
+  if (step === 11) {
   return (
     <div
       key="step11"
-      className={cn(step === 11 ? "flex flex-col" : "hidden", "w-full")}
+      className={cn("flex flex-col gap-4 mt-12 w-full")}
     >
-      <div className="flex flex-col w-full">
         <h3 className="text-xl">Congrats!</h3>
         <p>{stepData[11]?.blurb ?? "Sign up for an account to get started."}</p>
         <p>Learn more: </p>
-        <div className="flex flex-row justify-end">
+        <div className="flex justify-end">
           <a
             href="https://www.unkey.com/docs/introduction"
             target="_blank"
-            className="text-violet-400"
+            className="text-white"
           >
             <Button variant={"secondary"}>Docs</Button>
           </a>
           <a
             href="https://www.unkey.com"
             target="_blank"
-            className="text-violet-400"
+            className="text-white"
           >
             <Button variant={"default"}>Sign up</Button>
           </a>
-        </div>
-      </div>
+          </div>
     </div>
-  );
+  );} else {
+    return null;
+  }
 }
